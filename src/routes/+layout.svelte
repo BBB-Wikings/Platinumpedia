ddcbdec0978725c4efe6630fdd761310d729da88
Ricardo Cimma <ricardo.cimma@hayloft-it.ch>
2023-08-24 15:53:52 +0200

    Init firebase and flowbite

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import '../app.css';
+    import { FirebaseApp } from 'sveltefire';
+    import {auth, firestore, storage} from '$lib/firebase';
+</script>
+
+<FirebaseApp {auth} {firestore} {storage}>
+    <slot />
+</FirebaseApp>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { SignedIn, SignedOut } from 'sveltefire';
+    import { signInAnonymously } from "firebase/auth";
+    import { Alert } from 'flowbite-svelte';
+</script>
+
+<div class="p-8">
+    <Alert>
+        <span class="font-medium">Info alert!</span>
+        Change a few things up and try submitting again.
+    </Alert>
+</div>
+
+<SignedIn let:user let:signOut>
+    <p>Hello {user.uid}</p>
+    <button on:click={signOut}>Sign Out</button>
+</SignedIn>
+
+<SignedOut let:auth>
+    <button on:click={() => signInAnonymously(auth)}>Sign In</button>
+</SignedOut>
